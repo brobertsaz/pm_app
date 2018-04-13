01b4a4647dcfb19ffed2086d2876d4e9fbf631dd
Bob Roberts <cardsfanbob@gmail.com>
2018-04-12 23:54:13 -0500

    added axios and vuetify

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,12 @@ module PmApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.jbuilder false
+    end
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
